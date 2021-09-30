361fd0f83c770b94e1f0435802bdc7f18933fb01
Erwin Bofetiado <erwinjbofetiado@gmail.com>
2021-09-30 18:54:15 -0500

    grid traveler, table

--- dynamic-programming/canSum.js
+++ dynamic-programming/canSum.js
@@ -1,28 +1,34 @@
 // 7, [2,3]
 
-const canSum = (targetSum, numbers, memo = {}) => {
-  if (targetSum in memo) {
-    return memo[targetSum];
-  }
+// const canSum = (targetSum, numbers, memo = {}) => {
+//   if (targetSum in memo) {
+//     return memo[targetSum];
+//   }
 
-  if (targetSum < 0) {
-    return false;
-  }
+//   if (targetSum < 0) {
+//     return false;
+//   }
 
-  if (targetSum === 0) {
-    return true;
-  }
+//   if (targetSum === 0) {
+//     return true;
+//   }
 
-  for (const num of numbers) {
-    const remainder = targetSum - num;
-    const remainderCall = canSum(remainder, numbers, memo);
-    memo[remainder] = remainderCall;
-    if (remainderCall) {
-      return remainderCall;
-    }
-  }
+//   for (const num of numbers) {
+//     const remainder = targetSum - num;
+//     const remainderCall = canSum(remainder, numbers, memo);
+//     memo[remainder] = remainderCall;
+//     if (remainderCall) {
+//       return remainderCall;
+//     }
+//   }
 
-  return false;
-};
+//   return false;
+// };
 
-canSum(300, [7, 14]);
+// const canSum = (targetSum, numbers) => {
+
+// }
+
+//8, [4,3,2,1]
+
+// canSum(300, [7, 14]);
--- dynamic-programming/gridTraveler.js
+++ dynamic-programming/gridTraveler.js
@@ -1,5 +1,3 @@
-// (3,3)
-
 // const gridTraveler = (length, width, memo = {}) => {
 //   const coordinate = length + ',' + width;
 
@@ -26,10 +24,31 @@
 //   return memo[coordinate];
 // };
 
-const gridTraveler = (n, m) => {};
+const gridTraveler = (n, m) => {
+  const table = Array(n + 1)
+    .fill()
+    .map(() => Array(m + 1).fill(0));
 
-//[0,0,0]
-//[0,0,0]
-//[0,0,0]
+  table[1][1] = 1;
 
-gridTraveler(3, 2);
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= m; j++) {
+      const current = table[i][j];
+      if (j + 1 <= m) {
+        table[i][j + 1] += current;
+      }
+      if (i + 1 <= n) {
+        table[i + 1][j] += current;
+      }
+    }
+  }
+  return table[n][m];
+};
+
+//  c
+//r[0,0,0]
+// [0,1,0]
+// [0,0,0]
+// [0,0,0]
+
+gridTraveler(18, 18);
