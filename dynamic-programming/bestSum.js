3196d487a9f5a2af24cb4c2f529247733ed57737
Erwin Bofetiado <erwinjbofetiado@gmail.com>
2021-10-02 16:26:14 -0500

    howSum, bestSum table

--- dynamic-programming/bestSum.js
+++ dynamic-programming/bestSum.js
@@ -1,34 +1,58 @@
-const bestSum = (targetSum, numbers, memo = {}) => {
-  if (targetSum in memo) {
-    return memo[targetSum];
-  }
+// const bestSum = (targetSum, numbers, memo = {}) => {
+//   if (targetSum in memo) {
+//     return memo[targetSum];
+//   }
 
-  if (targetSum < 0) {
-    return null;
-  }
-  if (targetSum === 0) {
-    return [];
-  }
+//   if (targetSum < 0) {
+//     return null;
+//   }
+//   if (targetSum === 0) {
+//     return [];
+//   }
 
-  var resultArray = null;
+//   var resultArray = null;
 
-  for (const num of numbers) {
-    const remainder = targetSum - num;
+//   for (const num of numbers) {
+//     const remainder = targetSum - num;
 
-    const remainderResult = bestSum(remainder, numbers, memo);
+//     const remainderResult = bestSum(remainder, numbers, memo);
 
-    memo[remainder] = remainderResult;
+//     memo[remainder] = remainderResult;
 
-    if (remainderResult) {
-      const tempArray = [...remainderResult, num];
+//     if (remainderResult) {
+//       const tempArray = [...remainderResult, num];
 
-      if (resultArray === null || tempArray.length < resultArray.length) {
-        resultArray = tempArray;
+//       if (resultArray === null || tempArray.length < resultArray.length) {
+//         resultArray = tempArray;
+//       }
+//     }
+//   }
+
+//   return resultArray;
+// };
+
+// bestSum(100, [5, 3]);
+
+//7 , [5,4,3]
+
+const bestSum = (targetSum, numbers) => {
+  var table = Array(targetSum + 1).fill(null);
+
+  table[0] = [];
+
+  for (let i = 0; i <= targetSum; i++) {
+    if (table[i]) {
+      for (const num of numbers) {
+        //can be null, undefined, or [..combo]
+        const currentCombo = table[i + num];
+        const newCombo = [...table[i], num];
+        if (!currentCombo || newCombo.length < currentCombo.length) {
+          table[i + num] = newCombo;
+        }
       }
     }
   }
-
-  return resultArray;
+  return table[targetSum];
 };
 
-bestSum(100, [5, 3]);
+bestSum(100, [1, 2, 5, 25]);
--- dynamic-programming/howSum.js
+++ dynamic-programming/howSum.js
@@ -1,30 +1,45 @@
 //7, [5,3,4,7] => [7], [3,4]
 
-const howSum = (targetSum, numbers, memo = {}) => {
-  if (targetSum in memo) {
-    return memo[targetSum];
-  }
-  if (targetSum === 0) {
-    return [];
-  }
+// const howSum = (targetSum, numbers, memo = {}) => {
+//   if (targetSum in memo) {
+//     return memo[targetSum];
+//   }
+//   if (targetSum === 0) {
+//     return [];
+//   }
 
-  if (targetSum < 0) {
-    return null;
-  }
+//   if (targetSum < 0) {
+//     return null;
+//   }
 
-  for (const num of numbers) {
-    const remainder = targetSum - num;
+//   for (const num of numbers) {
+//     const remainder = targetSum - num;
 
-    const remainderResult = howSum(remainder, numbers, memo);
+//     const remainderResult = howSum(remainder, numbers, memo);
 
-    memo[num] = remainderResult;
+//     memo[num] = remainderResult;
 
-    if (remainderResult) {
-      return [...remainderResult, num];
+//     if (remainderResult) {
+//       return [...remainderResult, num];
+//     }
+//   }
+
+//   return null;
+// };
+
+const howSum = (targetSum, numbers) => {
+  var table = Array(targetSum + 1).fill(null);
+  table[0] = [];
+
+  for (let i = 0; i < targetSum; i++) {
+    if (table[i]) {
+      for (const num of numbers) {
+        table[i + num] = [...table[i], num];
+      }
     }
   }
 
-  return null;
+  return table[targetSum];
 };
 
-howSum(301, [7, 14]);
+howSum(7, [5, 3, 4]);
