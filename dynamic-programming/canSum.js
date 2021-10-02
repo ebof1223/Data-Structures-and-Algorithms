b8cb1644cfdcd492d0f42539fa789fe23538d4df
Erwin Bofetiado <erwinjbofetiado@gmail.com>
2021-10-02 14:48:50 -0500

    canSum table

--- dynamic-programming/canSum.js
+++ dynamic-programming/canSum.js
@@ -30,5 +30,20 @@
 // }
 
 //8, [4,3,2,1]
+//[0,1,2,3,4,5,6,7,8]
+const canSum = (targetSum, numbers) => {
+  var table = Array(targetSum + 1).fill(false);
+  table[0] = true;
+  for (let i = 0; i <= targetSum; i++) {
+    if (table[i] === true) {
+      for (let num of numbers) {
+        const idx = i + num;
+        table[idx] = true;
+      }
+    }
+  }
 
-// canSum(300, [7, 14]);
+  return table[targetSum];
+};
+
+canSum(300, [7, 14]);
