eb4f9ec0a56b788cb4a5bc4a1b7fd977a9736aaf
Erwin Bofetiado <erwinjbofetiado@gmail.com>
2021-10-06 16:14:10 -0500

    added recursion/combinations, coinChange2

---
+++ recursion/combinations.js
@@ -0,0 +1,17 @@
+const combinations = (array) => {
+  if (array.length === 0) return [[]];
+
+  const firstEl = array[0];
+  const rest = array.slice(1);
+
+  const withoutFirstEl = combinations(rest);
+  var withFirstElCombos = [];
+
+  withoutFirstEl.forEach((combo) => {
+    const withFirstElCombo = [...combo, firstEl];
+    withFirstElCombos.push(withFirstElCombo);
+  });
+  return [...withFirstElCombos, ...withoutFirstEl];
+};
+
+combinations([1, 2, 3]);
