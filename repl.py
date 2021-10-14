8daf2ec89235ac778e1e14da687a8c89f779705e
Erwin Bofetiado <erwinjbofetiado@gmail.com>
2021-10-14 12:38:58 -0500

    none

--- repl.py
+++
@@ -1,9 +0,0 @@
-# def missingNumber(nums) -> int:
-#     max_value = max(nums)
-#     nums_range = list(range(0, max_value + 1))
-#     for num in nums:
-#         nums_range.remove(num)
-#     return nums_range[0]
-
-
-# print(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
